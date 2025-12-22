452ca05ad5edd2a9bb9fccdfefb593b88a79e3c8
StephaneDock <stephane.dondyas@dock26.de>
2025-12-22 12:10:30 +0100

    cleaning up

--- src/main.ts
+++ src/main.ts
@@ -1,202 +1,194 @@
-import './style.css'
-import * as CookieConsent from 'vanilla-cookieconsent'
+import "./style.css";
+import * as CookieConsent from "vanilla-cookieconsent";
 
 /**
  * All config. options available here:
  * https://cookieconsent.orestbida.com/reference/configuration-reference.html
  */
 
-type Dock26CookieConsent = {
-    acceptedCategory: (category: string) => boolean;
-}
-
 type Dock26CookieConsentCategories = {
-    id: string;
-    name: string;
-    description: string;
-    enabled: boolean;
-    readOnly: boolean;
+  id: string;
+  name: string;
+  description: string;
+  enabled: boolean;
+  readOnly: boolean;
 };
 
 type Dock26CookieConsentSettings = {
-    [key: string]: any;
+  [key: string]: any;
 };
 
 declare global {
-    interface Window {
-        Dock26CookieConsent: Dock26CookieConsent;
-        dock26Cookies: {
-            settings: Dock26CookieConsentSettings;
-            categories: Dock26CookieConsentCategories[];
-        };
-    }
+  interface Window {
+    dock26Cookies: {
+      settings: Dock26CookieConsentSettings;
+      categories: Dock26CookieConsentCategories[];
+    };
+  }
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  if (window.dock26Cookies.categories.length === 0) {
+    console.warn("No consent categories found.");
+  }
 
-    if (window.dock26Cookies.categories.length === 0) {
-        console.warn('No consent categories found.');
-    }
+  const cookieValue = window.CookieConsent.getCookie();
+  const categories: CookieConsent.CookieConsentConfig["categories"] = {};
+  const sections: CookieConsent.Section[] = [];
+  const settings = window.dock26Cookies.settings;
 
-    const cookieValue = window.CookieConsent.getCookie()
-
-    const categories: CookieConsent.CookieConsentConfig['categories'] = {};
-    const sections: CookieConsent.Section[] = [];
-
-    window.dock26Cookies.categories.forEach(category => {
-        const categoryId = category.id.toString();
-        categories[categoryId] = {
-            enabled: category.enabled ? cookieValue.categories.includes(categoryId) : false,
-            readOnly: category.readOnly
-        };
-        sections.push({
-            title: category.name,
-            description: category.description,
-            linkedCategory: (category.enabled && category.readOnly) ? undefined : categoryId
-        });
+  window.dock26Cookies.categories.forEach((category) => {
+    const categoryId = category.id.toString();
+    categories[categoryId] = {
+      enabled: category.enabled
+        ? cookieValue.categories.includes(categoryId)
+        : false,
+      readOnly: category.readOnly,
+    };
+    sections.push({
+      title: category.name,
+      description: category.description,
+      linkedCategory:
+        category.enabled && category.readOnly ? undefined : categoryId,
     });
+  });
 
-    const settings = window.dock26Cookies.settings;
+  await CookieConsent.run({
+    // root: "body",
+    // autoShow: true,
+    disablePageInteraction: true,
+    // hideFromBots: true,
+    // mode: 'opt-in',
+    // revision: 0,
 
-    await CookieConsent.run({
+    cookie: {
+      name: "cc_cookie",
+      // domain: location.hostname,
+      // path: '/',
+      // sameSite: "Lax",
+      // expiresAfterDays: 182,
+    },
 
-        root: 'body',
-        // autoShow: true,
-        disablePageInteraction: true,
-        // hideFromBots: true,
-        // mode: 'opt-in',
-        // revision: 0,
+    // https://cookieconsent.orestbida.com/reference/configuration-reference.html#guioptions
+    guiOptions: {
+      consentModal: {
+        layout: "cloud inline",
+        position: "bottom center",
+        equalWeightButtons: true,
+        flipButtons: false,
+      },
+      preferencesModal: {
+        layout: "box",
+        equalWeightButtons: true,
+        flipButtons: false,
+      },
+    },
 
-        cookie: {
-            name: 'cc_cookie',
-            // domain: location.hostname,
-            // path: '/',
-            // sameSite: "Lax",
-            // expiresAfterDays: 182,
-        },
+    onFirstConsent: ({ cookie }) => {
+      console.log("First consent given:", cookie);
+    },
 
-        // https://cookieconsent.orestbida.com/reference/configuration-reference.html#guioptions
-        guiOptions: {
-            consentModal: {
-                layout: 'cloud inline',
-                position: 'bottom center',
-                equalWeightButtons: true,
-                flipButtons: false
-            },
-            preferencesModal: {
-                layout: 'box',
-                equalWeightButtons: true,
-                flipButtons: false
-            }
-        },
+    onConsent: ({ cookie }) => {
+      // handleConsent(cookie);
+      console.log("Consent updated:", cookie);
+    },
 
-        onFirstConsent: ({ cookie }) => {
-            console.log('First consent given:', cookie);
-        },
+    onChange: ({ changedCategories, changedServices }) => {
+      console.log("onChange fired!", changedCategories, changedServices);
+      handleConsentChange(changedCategories, changedServices);
+    },
 
-        onConsent: ({ cookie }) => {
-            // handleConsent(cookie);
-            console.log('Consent updated:', cookie);
-        },
+    onModalReady: ({ modalName }) => {
+      console.log("ready:", modalName);
+    },
 
-        onChange: ({ changedCategories, changedServices }) => {
-            console.log('onChange fired!', changedCategories, changedServices);
-            handleConsentChange(changedCategories, changedServices);
-        },
+    onModalShow: ({ modalName }) => {
+      console.log("visible:", modalName);
+    },
 
-        onModalReady: ({ modalName }) => {
-            console.log('ready:', modalName);
-        },
+    onModalHide: ({ modalName }) => {
+      console.log("hidden:", modalName);
+    },
 
-        onModalShow: ({ modalName }) => {
-            console.log('visible:', modalName);
-        },
+    categories: categories,
 
-        onModalHide: ({ modalName }) => {
-            console.log('hidden:', modalName);
-        },
-
-        categories: categories,
-
-        language: {
-            default: 'de',
-            translations: {
-                de: {
-                    consentModal: {
-                        title: settings.consent_modal_title || 'Cookie-Zustimmung',
-                        description: settings.consent_modal_description || 'Wir verwenden Cookies',
-                        acceptAllBtn: settings.consent_modal_accept_all_btn || 'Alle akzeptieren',
-                        acceptNecessaryBtn: settings.consent_modal_accept_necessary_btn || 'Nur notwendige akzeptieren',
-                        showPreferencesBtn: settings.consent_modal_show_preferences_btn || 'Individuelle Präferenzen verwalten',
-                        // closeIconLabel: 'Reject all and close modal',
-                        footer: `
+    language: {
+      default: "de",
+      translations: {
+        de: {
+          consentModal: {
+            title: settings.consent_modal_title || "Cookie-Zustimmung",
+            description:
+              settings.consent_modal_description || "Wir verwenden Cookies",
+            acceptAllBtn:
+              settings.consent_modal_accept_all_btn || "Alle akzeptieren",
+            acceptNecessaryBtn:
+              settings.consent_modal_accept_necessary_btn ||
+              "Nur notwendige akzeptieren",
+            showPreferencesBtn:
+              settings.consent_modal_show_preferences_btn ||
+              "Individuelle Präferenzen verwalten",
+            // closeIconLabel: 'Reject all and close modal',
+            footer: `
                         <a href="${settings.imprint_link}" target="_blank">Impressum</a>
                         <a href="${settings.privacy_policy_link}" target="_blank">Datenschutzerklärung</a>
                     `,
-                    },
-                    preferencesModal: {
-                        title: settings.preferences_modal_title || 'Cookie-Einstellungen',
-                        acceptAllBtn: settings.preferences_modal_accept_all_btn || 'Alle akzeptieren',
-                        acceptNecessaryBtn: settings.preferences_modal_accept_necessary_btn || 'Nur notwendige akzeptieren',
-                        savePreferencesBtn: settings.preferences_modal_save_preferences_btn || 'Aktuelle Auswahl akzeptieren',
-                        closeIconLabel: settings.preferences_modal_close_icon_label || 'Modal schließen',
-                        serviceCounterLabel: settings.preferences_modal_service_counter_label || 'Dienst|Dienste',
-                        sections: [
-                            ...sections
-                        ]
-                    }
-                }
-            }
-        }
+          },
+          preferencesModal: {
+            title: settings.preferences_modal_title || "Cookie-Einstellungen",
+            acceptAllBtn:
+              settings.preferences_modal_accept_all_btn || "Alle akzeptieren",
+            acceptNecessaryBtn:
+              settings.preferences_modal_accept_necessary_btn ||
+              "Nur notwendige akzeptieren",
+            savePreferencesBtn:
+              settings.preferences_modal_save_preferences_btn ||
+              "Aktuelle Auswahl akzeptieren",
+            closeIconLabel:
+              settings.preferences_modal_close_icon_label || "Modal schließen",
+            serviceCounterLabel:
+              settings.preferences_modal_service_counter_label ||
+              "Dienst|Dienste",
+            sections: [...sections],
+          },
+        },
+      },
+    },
+  });
+
+  function handleConsentChange(
+    changedCategories: string[],
+    _changedServices: {
+      [key: string]: string[];
+    }
+  ) {
+    const cookieValue = window.CookieConsent.getCookie();
+    // Check for each changed categories if the Consent ist given or revoked
+    // If a consent has been removed, reload the page
+    let shouldReload = false;
+    changedCategories.forEach((category) => {
+      console.log(category, cookieValue.categories);
+      if (!cookieValue.categories.includes(category)) {
+        shouldReload = true;
+      }
     });
 
-    function handleConsentChange(changedCategories: string[], changedServices: {
-        [key: string]: string[];
-    }) {
-        console.log('Consent changed!', changedCategories, changedServices);
-        // Find all elements that requires consent
-        // const consentElements = document.querySelectorAll('[data-consent]');
-        // try {
-        //     consentElements.forEach(element => {
-        //         const consentRequired = element.getAttribute('data-consent');
-        //         if (consentRequired && CookieConsent.acceptedCategory(consentRequired)) {
-        //             // Show the element
-        //             element.classList.remove('hidden');
-        //         } else {
-        //             // Reload the page
-        //             location.reload();
-        //             throw new Error('Consent not given');
-        //         }
-        //     });
-        // } catch (error) {
-        //     console.log(error);
-        // }
+    if (shouldReload) {
+      window.location.reload();
     }
+  }
 
-    // Init Triggers Buttons
-    const initTriggers = () => {
-        const button = document.getElementById('dock-26-cookies-trigger-cc')
-        if (!button) {
-            return;
-        }
-        button.addEventListener('click', () => {
-            console.log('Cookie consent button clicked');
-            CookieConsent.showPreferences();
-        });
-    };
-
-
-    initTriggers();
-
-    // Expose Dock26CookieConsent
-    const c: Dock26CookieConsent = {
-        acceptedCategory: (category: string) => {
-            return CookieConsent.acceptedCategory(category);
-        }
+  // Init Triggers Buttons
+  const initTriggers = () => {
+    const button = document.getElementById("dock-26-cookies-trigger-cc");
+    if (!button) {
+      return;
     }
+    button.addEventListener("click", () => {
+      console.log("Cookie consent button clicked");
+      CookieConsent.showPreferences();
+    });
+  };
 
-    // Extends Window interface
-
-
-    window.Dock26CookieConsent = c;
-})
+  initTriggers();
+});
