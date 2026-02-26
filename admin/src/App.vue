43c89febc7001c8358497cfe4ea6d400df28ef1c
StephaneDock <stephane.dondyas@dock26.de>
2026-02-26 11:36:36 +0100

    Routing

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import Navbar from '@/components/Navbar.vue';
-const count = ref(0)
+import Home from '@/pages/index.vue';
+import Settings from '@/pages/settings.vue';
+
+const currentPath = ref<string>(window.location.hash || '/')
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+  console.log('Current path:', currentPath.value) // Debugging log
+})
+
+const currentView = computed(() => {
+  const path = currentPath.value.replace('#', '') || '/'
+  switch (path) {
+    case '/':
+      return Home
+    case '/settings':
+      return Settings
+    default:
+      return Home
+  }
+})
+
 </script>
 
 <template>
     <Navbar />
     <h1>Dock26 Cookies Admin</h1>
-    <div class="card">
-        <button @click="count++">Count is {{ count }}</button>
-    </div>
+    <component :is="currentView" />
 </template>
--- admin/src/components/Navbar.vue
+++ admin/src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <a href="#">Home</a> |
-        <a href="#about">About</a> |
-        <a href="#non-existent-path">Broken Link</a>
+        <a href="#/">Home</a> |
+        <a href="#/settings">About</a> |
+        <a href="#/non-existent-path">Broken Link</a>
     </div>
 </template>
 
